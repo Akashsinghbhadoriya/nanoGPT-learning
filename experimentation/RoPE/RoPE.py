ac9dce1bfb9db855e005194d0743272b616d7da3
Akashsinghbhadoriya <akashbhadoriya00@gmail.com>
2026-05-31 19:51:22 +0530

    RoPE Implementation in GPT 2

--- experimentation/RoPE/RoPE.py
+++ experimentation/RoPE/RoPE.py
@@ -4,6 +4,7 @@
 
 import torch
 import matplotlib.pyplot as plt
+import torch.nn as nn
 
 # even numbered column i
 def sinewave(pos, i, d):
@@ -17,6 +18,47 @@ def RoPE_2d(sinx,cosx, x1, x2):
     x_axis = x1 * cosx - x2 * sinx
     y_axis = x1 * sinx + x2 * cosx
     return x_axis, y_axis
+
+class RotaryEmbedding(nn.Module):
+    def __init__ (self, dim, max_position_embedding = 4096, base = 10000):
+        super().__init__()
+        self.dim = dim
+
+        # inverse frequency for all the pairs of columns along the dimension
+        inv_freq = 1 / (torch.pow( base , torch.arange(0, dim, 2).float() / dim)) # (dim/2) 1D
+        # persistent prevents the buffer from being saved in the model state dict
+        self.register_buffer("inv_freq", inv_freq, persistent = False)
+
+        #precompute the positional angles upto max context length
+        t = torch.arange(max_position_embedding, dtype = torch.float32) #(pos_emb) 1D
+
+        # takes 2 1D matrix and create a 2D matrix where every value of 1 vector is multiplied with every value of the 2nd vector
+        freqs = torch.outer(t, self.inv_freq) #(pos_emb, dim/2)
+
+        #duplicate frequencies to match the dimensions head
+        emb = torch.cat((freqs, freqs), dim=-1) #(pos_emb, dim)
+
+        # Cache Cosine and Sine values (unsqueezed for batch/head broadcasting)
+        self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent = False)
+        self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent = False)
+
+    def forward(self, x, seq_len):
+        return self.cos_cached[:, :, :seq_len, :], self.sin_cached[:, :, :seq_len, :]
+        
+
+def rotate_half(x):
+    # split the tensor along the last dimension and rotates the half.
+    x1 = x[..., :x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+
+    return torch.cat((-x2, x1), dim=-1)
+
+def apply_rotary_pos_emb(q, k, sin, cos):
+    # rotary formula q = q * cos + rotate_half(q) * sin
+    q_rotated = q * cos + rotate_half(q) * sin # dimensions (b, nh, T, hs) * (:,:,T, hs) + (b, nh, T, hs) * (:,:,T, hs)
+    k_rotated = k * cos + rotate_half(k) * sin
+
+    return q_rotated, k_rotated # (b, nh, T, hs)
 
 def plotwavesign(x_axis, y_axis):
     plt.figure(figsize=(6, 6))
@@ -82,7 +124,7 @@ if __name__ == "__main__" :
         input_x.append([0.1 * pos, 0.1 * pos])
     input_x = torch.tensor(input_x)
 
-    print(input_x)
+    # print(input_x)
 
     rope_x = []
     rope_y = []
@@ -95,4 +137,22 @@ if __name__ == "__main__" :
         rope_y.append(b)
     
     # this gives a proper pattern to the graph instead of random values
-    plotwavesign(rope_x, rope_y)
+    # plotwavesign(rope_x, rope_y)
+
+    a = torch.arange(1, 101).reshape(10, 10)
+    a = torch.tensor(a)
+    print(a)
+    print(rotate_half(a))
+
+    # testing the rotary positional embedding
+    rope = RotaryEmbedding(10)
+
+    cos , sin = rope(a, 10)
+    q = a.view(1, 1, *a.shape)
+    k = a.view(1, 1, *a.shape)
+    print(q.shape)
+    print("cos:\n",cos)
+    print("sin:\n",sin)
+    q, k = apply_rotary_pos_emb(q, k, sin, cos)
+
+    print(q)
